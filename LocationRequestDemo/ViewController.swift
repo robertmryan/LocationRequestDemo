c8c8e4c14fb7e7bd43381813308f0d7846afca01
Robert Ryan <robert.ryan@mindspring.com>
2020-08-14 21:09:23 -0700

    Initial Commit

---
+++ LocationRequestDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationRequestDemo
+//
+//  Created by Robert Ryan on 8/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
